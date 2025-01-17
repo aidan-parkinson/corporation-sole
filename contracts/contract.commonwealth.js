3ea4bdf66a880329743ff6b1ee9f279b0521ec0e
Aidan Parkinson <aidan.parkinson@atkinsglobal.com>
2025-01-17 16:12:04 +0000

    smart contract

---
+++ contracts/contract.commonwealth.js
@@ -0,0 +1,26 @@
+application: {
+    peer : Organization(MCOP),
+    client : Organization(COP),
+    date.from : setUTCDate(),
+    date.to : setUTCDate(),
+    commonwealth.enforcement.value : number,
+    commonwealth.enforcement.unit : Intl.NumberFormat.currency(),
+    commonwealth.activity.value : number,
+    update(observation.commonwealth, peer,
+    client,
+    date.from,
+    date.to,
+    commonwealth.enforcement.value,
+    commonwealth.enforcement.unit,
+    commonwealth.activity.value);
+},
+observation.commonwealth contract: {
+    update(observation.commonwealth):
+        put(observation.commonwealth);
+        return(observation.commonwealth);
+        return(principles);
+},
+observation.commonwealth interface: {
+    Transactions : update;
+    Endorsement Policy : Any Organization(MCOP);
+}
