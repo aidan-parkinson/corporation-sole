0aa2a73ce87db59343836f4a024abdc5d4875abc
Aidan Parkinson <aidan.parkinson@atkinsglobal.com>
2025-01-17 15:54:11 +0000

    prep for hyperledger contracts

---
+++ contracts/contract.service.js
@@ -0,0 +1,29 @@
+application: {
+    peer : Organization(MCOP),
+    client : Organization(COP),
+    date.from : setUTCDate(),
+    date.to : setUTCDate(),
+    service.expenditure.value : number,
+    service.expenditure.unit : Intl.NumberFormat.currency(),
+    service.activity.value : number,
+    service.identity : Guid,
+    service.owner : Organization,
+    update(observation.service.performance, peer,
+        client,
+        date.from,
+        date.to,
+        service.expenditure.value,
+        service.expenditure.unit,
+        service.activity.value,
+        service.identity,
+        service.owner);
+},
+observation.service contract: {
+    update(observation.service):
+        put(observation.service);
+        return(observation.service);
+},
+observation.service.performance interface: {
+    Transactions : update;
+    Endorsement Policy : Any observation.service.owner(MCOP);
+}
