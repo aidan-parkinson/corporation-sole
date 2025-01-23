f0f70a1f0ac9714ded1d195898f1623f1be6d8ea
aidan-parkinson <58667937+aidan-parkinson@users.noreply.github.com>
2025-01-23 15:42:29 +0000

    Update ecosystem.js

--- definitions/ecosystem.js
+++ definitions/ecosystem.js
@@ -326,8 +326,6 @@ func (s *SmartContract) CreateEcosystem(ctx contractapi.TransactionContextInterf
     activity.value int,
     enforcement.value int,
     enforcement.unit string,
-    performance.value int,
-    performance.unit string,
     time.to int,
     time.from int)
   
